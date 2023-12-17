c91b9897e21670fef4fdb00075be39896594b142
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-17 18:59:21 +0500

    Setup Login screen

---
+++ screens/Login.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const Login = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Login Page</Text>
+    </View>
+  );
+};
+
+export default Login;
