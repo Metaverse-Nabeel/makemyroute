72d9c1dad558cda6adb9c3228280d9337342961c
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-20 23:41:25 +0500

    Add destination card component

--- components/DestinationCard.jsx
+++ components/DestinationCard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Text, View, TouchableOpacity, Image } from "react-native";
+import { Text, TouchableOpacity, ImageBackground, View } from "react-native";
 
-const DestinationCard = () => {
+const DestinationCard = ({ destImage, destTitle }) => {
+  const handleClick = () => {
+    
+  }
   return (
-    <View className="">
-      <TouchableOpacity className="w-[80%] h-[30%] bg-white rounded-lg">
-        <Image
-          source={require("../assets/images/app-start.png")}
-          resizeMode="cover"
-          className="flex-1 flex-col justify-center items-center"
-        />
-        <Text className="bg-white py-2 font-bold">Faisal Mosque</Text>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity className="flex-1" onPress={handleClick}>
+      <View className="rounded-xl m-6 bg-white">
+        <View className="flex justify-center items-center rounded-xl">
+          <ImageBackground
+            source={{ uri: destImage }}
+            resizeMode="cover"
+            className="w-full h-[200] rounded-xl"
+          />
+        </View>
+        <Text className="font-bold text-2xl text-center py-2">{destTitle}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
