d2bc240ffc1ee6d950d3e80aff310710aae99e0e
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-20 23:41:00 +0500

    Complete Destination card

--- screens/Destinations.jsx
+++ screens/Destinations.jsx
@@ -1,50 +1,65 @@
-import { View, Text } from "react-native";
+import { View, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import DestinationCard from "../components/DestinationCard";
 
 const destinations = [
   {
     id: 1,
-    image: "../assets/images/app-start.png",
-    title: "Faisal Mosque",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
+    title: "Badshahi Mosque",
   },
   {
     id: 2,
-    image: "../assets/images/app-start.png",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
     title: "Faisal Mosque",
   },
   {
     id: 3,
-    image: "../assets/images/app-start.png",
-    title: "Faisal Mosque",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
+    title: "Mahabat Khan Mosque",
   },
   {
     id: 4,
-    image: "../assets/images/app-start.png",
-    title: "Faisal Mosque",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
+    title: "Umer Mosque",
   },
   {
     id: 5,
-    image: "../assets/images/app-start.png",
-    title: "Faisal Mosque",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
+    title: "Ali Mosque",
   },
   {
     id: 6,
-    image: "../assets/images/app-start.png",
-    title: "Faisal Mosque",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
+    title: "Noor Mosque",
   },
   {
     id: 7,
-    image: "../assets/images/app-start.png",
-    title: "Faisal Mosque",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg/1024px-Hunza_Valley%2C_view_from_Eagle%27s_Nest.jpg",
+    title: "Haram Mosque",
   },
 ];
 
 const Destinations = () => {
   return (
-    <View className="flex flex-col justify-center items-center">
-      <DestinationCard />
-    </View>
+    <SafeAreaView className="flex-1">
+      <ScrollView className="bg-gray-200">
+        {destinations.map((destination) => (
+          <DestinationCard
+            key={destination.id}
+            destImage={destination.image}
+            destTitle={destination.title}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
