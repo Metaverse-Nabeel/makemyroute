cc4bc42f42e15cb7dd54c660fa5b300d4ab1f957
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-12-21 13:55:21 +0500

    Add Destination card

--- components/DestinationCard.jsx
+++ components/DestinationCard.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { Text, TouchableOpacity, ImageBackground, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const DestinationCard = ({ destImage, destTitle }) => {
-  const handleClick = () => {
-    
-  }
+const DestinationCard = (props) => {
+  const navigation = useNavigation();
+  const handleCardPress = () => {
+    // Navigate to the "Details" screen
+    navigation.navigate("Details", {
+      cardInfo: {
+        title: props.destData.title,
+        image: props.destData.image,
+        desc: props.destData.desc,
+        loc: props.destData.loc,
+        // Add other card information as needed
+      },
+    });
+  };
   return (
-    <TouchableOpacity className="flex-1" onPress={handleClick}>
+    <TouchableOpacity className="flex-1" onPress={handleCardPress}>
       <View className="rounded-xl m-6 bg-white">
         <View className="flex justify-center items-center rounded-xl">
           <ImageBackground
-            source={{ uri: destImage }}
+            source={{ uri: props.destData.image }}
             resizeMode="cover"
             className="w-full h-[200] rounded-xl"
           />
         </View>
-        <Text className="font-bold text-2xl text-center py-2">{destTitle}</Text>
+        <Text className="font-bold text-2xl text-center py-2">
+          {props.destData.title}
+        </Text>
       </View>
     </TouchableOpacity>
   );
